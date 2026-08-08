1527e9958deb84edf9276db36653e51069276890
mike41907 <mike41907@users.noreply.github.com>
2026-08-08 17:00:06 +0800

    fix responsive layout overflow

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'lexcore-v0.10.0'
+const CACHE_NAME = 'lexcore-v0.10.1'
 const SCOPE_URL = self.registration.scope
 const INDEX_URL = new URL('index.html', SCOPE_URL).href
 const APP_SHELL = [
