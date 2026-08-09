f5222cc291f4000362933896775f3aaef4526a5d
mike41907 <mike41907@users.noreply.github.com>
2026-08-09 18:15:19 +0800

    fix: improve mobile precision training reading layout

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'lexcore-v0.14.6'
+const CACHE_NAME = 'lexcore-v0.14.7'
 const SCOPE_URL = self.registration.scope
 const INDEX_URL = new URL('index.html', SCOPE_URL).href
 const APP_SHELL = [
